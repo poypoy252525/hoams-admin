a765f20e8f69b2f91f121fec1b9d87cd30494ad7
poypoy252525 <61503297+poypoy252525@users.noreply.github.com>
2022-11-05 09:00:42 +0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,49 @@
+const passToggler = document.querySelectorAll(".password-toggler");
+
+passToggler.forEach(item => {
+    item.addEventListener("click", function() {
+        const input = document.getElementById(item.getAttribute("data-password"));
+        input.focus();
+        item.classList.toggle("fa-eye");
+        item.classList.toggle("fa-eye-slash");
+        if (item.classList.contains("fa-eye-slash")) {
+            input.setAttribute("type", "text");
+        } else {
+            input.setAttribute("type", "password");
+        }
+    });
+});
+
+// sidenav
+
+const sideToggler = document.querySelectorAll('.sidenav-toggler');
+sideToggler.forEach(item => {
+    item.addEventListener('click', function() {
+        const sidebar = document.getElementById(item.getAttribute('data-toggle'));
+        sidebar.classList.toggle('show');
+    });
+});
+
+const sidebarLayer = document.getElementById('sidebar');
+sidebarLayer.addEventListener('click', function(e) {
+    if (e.target.matches('.sidenav-layer')) {
+        if (sidebarLayer.classList.contains('show')) {
+            sidebarLayer.classList.remove('show');
+        }
+    }
+});
+
+const minWidth1024px = window.matchMedia('(min-width: 1024px)');
+if (minWidth1024px.matches) {
+    sidebarLayer.classList.add('show');
+} else {
+    sidebarLayer.classList.remove('show');
+}
+minWidth1024px.addListener(function() {
+    if (minWidth1024px.matches) {
+        sidebarLayer.classList.add('show');
+    } else {
+        sidebarLayer.classList.remove('show');
+    }
+});
+
